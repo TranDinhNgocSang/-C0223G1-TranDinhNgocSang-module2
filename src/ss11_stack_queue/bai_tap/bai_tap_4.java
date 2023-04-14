cd13e0104c05664e9211672ac47ac494ca127934
TranDinhNgocSang <sangdolphin98@gmail.com>
2023-04-14 23:53:19 +0700

    14/03/2023

--- src/ss11_stack_queue/bai_tap/bai_tap_4.java
+++ src/ss11_stack_queue/bai_tap/bai_tap_4.java
@@ -1,29 +1,34 @@
 package ss11_stack_queue.bai_tap;
 
-import java.util.Objects;
 import java.util.Stack;
 
 public class bai_tap_4 {
     public static void main(String[] args) {
-        Stack<String> symOpen = new Stack<>();
-        Stack<String> symClose = new Stack<>();
-        String input = "a*(b-c(s+b))";
-        String str[] = input.split("");
+        String str = "a*(b-c(s+b))";
+        System.out.println(str);
+        checkBracket(str);
+    }
+
+    public static void checkBracket(String str) {
+        Stack<String> stack1 = new Stack<>();
+        Stack<String> stack2 = new Stack<>();
+        String strings[] = str.split("");
         boolean flag = true;
-        for (int i = 0; i < str.length; i++) {
-            if (Objects.equals(str[i], "(")) {
-                symOpen.push(str[i]);
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].equals("(")) {
+
+                stack1.push(strings[i]);
             }
-            if (Objects.equals(str[i], ")")) {
-                symClose.push(str[i]);
+            if (strings[i].equals(")")) {
+                stack2.push(strings[i]);
             }
-            if (symOpen.size() < symClose.size()) {
+            if (stack1.size() < stack2.size()) {
                 flag = false;
                 break;
             }
         }
-        if (flag && symOpen.size() == symClose.size()) {
-            System.out.println("ok");
+        if (flag && stack1.size() == stack2.size()) {
+            System.out.println("oke");
         } else System.out.println("no");
     }
 }
