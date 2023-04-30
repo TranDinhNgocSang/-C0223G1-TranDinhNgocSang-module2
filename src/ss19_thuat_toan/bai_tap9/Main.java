13594f67c832af90d57a671e09fe9b56a57a0c43
TranDinhNgocSang <sangdolphin98@gmail.com>
2023-04-30 20:05:35 +0700

    30/04/2023

--- src/ss19_thuat_toan/bai_tap9/Main.java
+++ src/ss19_thuat_toan/bai_tap9/Main.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {9,6,3,1,4,7,9,2,0,4,9,5,7};
+        int[] arr = {9, 6, 3, 1, 4, 7, 9, 2, 0, 4, 9, 5, 7};
         System.out.println(Arrays.toString(arr));
         Arrays.sort(arr);
-        for (int i = arr.length-1; i >=0 ; i--) {
-            if (arr[i]<arr[arr.length-1]){
-                System.out.println("số lớn thứ 2 trong mảng là: "+arr[i]);
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] < arr[arr.length - 1]) {
+                System.out.println("số lớn thứ 2 trong mảng là: " + arr[i]);
                 break;
             }
         }
